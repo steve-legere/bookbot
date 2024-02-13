b883ae4e948471d89dd58ad7f0d3815259007d9c
steve-legere <stevelegere@gmx.com>
2024-02-13 15:20:31 -0500

    implemented report printing

--- main.py
+++ main.py
@@ -5,26 +5,28 @@ if version_info < (3, 9):
     exit("Python 3.9 or higher is required to run this script.")
 
 def init_parser():
-    # Create ArgumentParser object
     parser = argparse.ArgumentParser(description='Provides word and character reports on a text document.')
-
-    # Add arguments
     parser.add_argument('file', type=str, help='File path of book to parse')
-
-    # Parse the arguments
     args = parser.parse_args()
-
     return parser, args
 
 def process_text(text):
     words = text.split()
     chars = {}
     for char in text:
-        if char in chars:
-            chars[char] += 1
+        if char.lower() in chars:
+            chars[char.lower()] += 1
         else:
-            chars[char] = 1
-    return words, chars
+            chars[char.lower()] = 1
+    sorted_chars = dict(sorted(chars.items(), key=lambda item: item[1], reverse=True))
+    return words, sorted_chars
+
+def print_report(name, words, chars):
+    print(f"--- Begin report of {name} ---")
+    print(f"{len(words)} words found in the document\n")
+    for char, num in chars.items():
+        print(f"The {repr(char)} character was found {num} times")
+    print("--- End report ---")
 
 def main():
     parser, args = init_parser()
@@ -32,7 +34,7 @@ def main():
         with open(args.file) as f:
             text = f.read()
             words, chars = process_text(text)
-            print(f"There are {len(words)} words and {len(chars)} characters.")      
+            print_report(args.file, words, chars)    
     except FileNotFoundError as e:
         exit(e)
 
